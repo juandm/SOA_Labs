de37a267b10d14786b9e554bcf9817e0d34b06c1
Juan David Méndez <juand.mendez28@gmail.com>
2017-10-09 15:42:17 -0300

    deleting some folders

--- External_Services/productService/src/br/ufsc/das/productservice/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://das.ufsc.br/ProductService/")
-package br.ufsc.das.productservice;
--- External_Services/sumService/src/br/ufsc/das/sumservice/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://das.ufsc.br/SumService/")
-package br.ufsc.das.sumservice;
